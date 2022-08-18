7171acfed76762f65f17c3ad1b34c1546d638315
«Roman» <orlusha8@mail.ru>
2022-08-18 20:36:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Console.WriteLine("\"Khal Drogo's favorite word is \"athjahakar\"\"");
+//Console.WriteLine("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");
+//Console.WriteLine("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey.");
+/* var motto = "What Is Dead May Never Die!";
+Console.WriteLine(motto); */
+
+/*var name = "Brienna";
+
+// BEGIN (write your solution here)
+name = "Brienna = var name";
+// END
+
+Console.WriteLine(name);*/
+/*
+var eurosCount = 100;
+var dollarsCount = 100 * 1.25;
+var rublesCount = dollarsCount * 60;
+Console.WriteLine(dollarsCount + ".0");
+Console.WriteLine(rublesCount + ".0");
+*/
+/*
+var info = "We couldn't verify you mother's maiden name.";
+var intro = "Here is important information about your account security.";
+
+var firstName = "Joffrey";
+var greeting = "Hello";
+Console.WriteLine(greeting + ", " + firstName + "!");
+Console.WriteLine(intro + "\n" + info);
+*/
+/*
+var myFirstNumber = 11;
+var mySecondNumber = -100;
+Console.WriteLine(myFirstNumberVar * mySecondNumberVar);
+*/
+
+/*
+var king = "King Balon the 6th";
+var firstRoom = 6;
+var secondRoom = 17;
+Console.WriteLine(king + " has " + (firstRoom * secondRoom) + " rooms.");
+*/
